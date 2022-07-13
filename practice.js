5a881c311bcfcf555c01cda07803bb4b6d407bbb
Courtney S <smithcm11@yahoo.com>
2022-07-13 10:50:37 -0400

    still working

--- practice.js
+++ practice.js
@@ -6,15 +6,27 @@ let daysToSat;
 switch (day) {
     case 'sunday':
         daysToSat = 6;
+        break; 
     case 'monday':
         daysToSat = 5;
+        break; 
     case 'tuesday':
         daysToSat = 4;
+        break; 
     case 'wednesday':
         daysToSat = 3;
+        break; 
     case 'thursday':
         daysToSat = 2;
-
+        break; 
+    case 'friday':
+        daysToSat = 1;
+        break; 
+    case 'saturday':
+        daysToSat = 0;
+        break; 
+    default:
+        daysToSat = null;
 
 
 
