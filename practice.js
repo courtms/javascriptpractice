e8a9b93cf28f61e12af3988e8485f87221a95938
Courtney S <smithcm11@yahoo.com>
2022-07-13 10:47:02 -0400

    more code

--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
 let day = 'Wednesday';
+
+day = day.toLocaleLowerCase();
+let daysToSat; 
+
+switch (day) {
+    case 'sunday':
+        daysToSat = 6;
+    case 'monday':
+        daysToSat = 5;
+    case 'tuesday':
+        daysToSat = 4;
+    case 'wednesday':
+        daysToSat = 3;
+    case 'thursday':
+        daysToSat = 2;
+
+
+
+
+
+
+
+
+
+
+}
